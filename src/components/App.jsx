34f24799ecf2fcb106089d5c466792c0a2043994
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2023-12-15 16:03:37 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,8 +10,8 @@ export const App = () => {
   return (
   <Routes>
     <Route path='/' element={<Home />} />
-    <Route path='/movies' element={<Movies />} />
-    <Route path="/movies/:movieId" element={<MovieDetails />}>
+    <Route path='movies' element={<Movies />} />
+    <Route path="movies/:movieId" element={<MovieDetails />}>
         <Route path="cast" element={<Cast />} />
         <Route path="reviews" element={<Reviews />} />
       </Route>
