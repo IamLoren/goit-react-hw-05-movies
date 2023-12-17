d37e5b94af5e4efcab31890e486ae286e1de5e48
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2023-12-17 11:11:59 +0200

    added movie-information

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,17 +1,43 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
 import { Outlet, useParams} from 'react-router-dom';
 import Header from '../components/Header/Header.jsx';
+import { getAllMovies } from 'services/api.js';
 
 const MovieDetails = () => {
 
 const {movieId} = useParams();
+const [result, setResult] = useState({});
 
+useEffect(()=> {
+  const getMovies = async () => {
+    try {
+      const res = await getAllMovies(`/movie/${movieId}`);
+      console.log(res)
+      setResult(res)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  getMovies()
+}, [movieId])
+
+const {original_title, release_date, poster_path,  overview} = result;
 
   return (
     <>
       <Header />
-      <button>Go back</button>
-
+      
+      <section>
+        <button>Go back</button>
+        <div className="generalInformation">
+          <img src={poster_path ? `https://image.tmdb.org/t/p/original${poster_path}` : 'https://img.freepik.com/free-vector/shot-concept-illustration_114360-7065.jpg?w=826&t=st=1702759958~exp=1702760558~hmac=ac147e6f1032e40ad076c4ad24885e18d2ca16dddb473b0fbb77c6911c57f43e'} alt="poster" width='300' height='470' loading='lazy' className="poster" />
+          <div className="description">
+            <h1 className="title">{original_title}</h1>
+            <p>{overview}</p>
+            <p className="release">Release date: {release_date}</p>
+          </div>
+        </div>
+      </section>
       <Outlet />
     </>
   );
--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 const KEY = '25d5768f21131594e931abf62b991011';
 
-export const getAllMovies = async (endpoint, query) => {
+export const getAllMovies = async (endpoint, query='') => {
   const params = new URLSearchParams({
     api_key: KEY,
     append_to_response: 'videos',
